973fd291832545389041f21668e285de15710e0c
Adir Dayan <adirdayan@gmail.com>
2020-06-04 14:21:53 +0200

    Updates

---
+++ precache-manifest.ccdc26ac59af77b2f3443e0932aef9b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193a363c3197fa6fd253c2474c4b7f51",
+    "url": "/rick-and-mortypedia/index.html"
+  },
+  {
+    "revision": "d43ae644a301e8dd6541",
+    "url": "/rick-and-mortypedia/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "500eab252f53b969de09",
+    "url": "/rick-and-mortypedia/static/css/main.4f8130e2.chunk.css"
+  },
+  {
+    "revision": "d43ae644a301e8dd6541",
+    "url": "/rick-and-mortypedia/static/js/2.83e175bf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/rick-and-mortypedia/static/js/2.83e175bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500eab252f53b969de09",
+    "url": "/rick-and-mortypedia/static/js/main.5ae86a02.chunk.js"
+  },
+  {
+    "revision": "fac27fe18055ef3d4eab",
+    "url": "/rick-and-mortypedia/static/js/runtime-main.71e0ec05.js"
+  }
+]);
